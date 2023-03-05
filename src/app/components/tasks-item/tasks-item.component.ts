489972ac5cdb895cda16c02535bccc7cdf079f0f
unknown <nicolasramirez22498@gmail.com>
2023-03-05 15:01:34 -0300

    loco

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-task';
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+  @Input() taks: Task = TASKS[0]
+
+}
